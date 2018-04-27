248ab6a6bb29ac8a6052484df207c98da8477326
ulag-india <ulag.india@gmail.com>
2018-04-28 01:08:09 +0530

    README file has been updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# soap-number-conversion" 
+# soap-number-conversion
+
+# Stub is generated using the below command
+
+# wsimport -keep -s src http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL
+
+# Package com.dataaccess.webservicesaserver is created to place the source files.
+
--- src/com/mypackage/soap/NumberConversionApp.java
+++ src/com/mypackage/soap/NumberConversionApp.java
@@ -6,9 +6,6 @@ import com.dataaccess.webservicesserver.NumberConversion;
 
 public class NumberConversionApp {
 
-	// Stub is generated using the below command
-	// wsimport -keep -s src http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL
-	// Package com.dataaccess.webservicesaserver is created to place the source files.
 	public static void main(String[] args) {
 		BigInteger inputBig = new BigInteger("4321");
 		NumberConversion numberConversion = new NumberConversion();
